1e5ea4020b9809bb8725fbe476a33f9f1f96f35f
shajibalmamun@gmail.com <shajibalmamun@gmail.com>
2022-04-06 02:56:37 +0600

    update file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import fs from 'fs';
 
 class userController {
     static createUser = async(req, res) => {
@@ -24,6 +25,64 @@ class userController {
                message: error.message,
                error: true
            }) 
+        }
+
+    };
+
+    static updateUserById =  async(req, res) => {
+        const id =  req.params.id;
+        let reqBody = req.body;
+
+        var imageUrl = '';
+        if(req.file) var imageUrl = `storage/images/${req.file.filename}`;
+        reqBody.avater = imageUrl;
+
+        try {
+            const userInfo = await User.findById(id);
+            const userPhotoInfo = userInfo.avater;
+            if(userPhotoInfo){
+                fs.unlinkSync(DIR + userPhotoInfo);
+            }
+            const updateIteam = await User.findByIdAndUpdate({_id: id}, reqBody);
+            res.status(201).json({
+                status_code: 201,
+                message: "update Successfully!!",
+                data: updateIteam
+            }) 
+            
+        } catch (error) {
+          console.log(error);  
+          res.status(501).json({
+            status_code: 501,
+            message: error.message,
+            error: true
+        }) 
+        }
+
+    }
+
+    static deleteUserById =  async(req, res) => {
+        const id =  req.params.id;
+        try {
+            const userInfo = await User.findById(id);
+            const userPhotoInfo = userInfo.avater;
+            if(userPhotoInfo){
+                fs.unlinkSync(DIR + userPhotoInfo);
+            }
+            const userDelete = await User.findByIdAndDelete({_id: id});
+            res.status(201).json({
+                status_code: 201,
+                message: "update Successfully!!",
+                data: updateIteam
+            }) 
+            
+        } catch (error) {
+          console.log(error);  
+          res.status(501).json({
+            status_code: 501,
+            message: error.message,
+            error: true
+        }) 
         }
 
     }
--- router/api.js
+++ router/api.js
@@ -5,6 +5,9 @@ import fileUpload from "../utils/fileUpload.js";
 const router = express.Router();
 
 router.post('/user', fileUpload("./storage/images"), userController.createUser);
+router.post('/update-user/:id', fileUpload("./storage/images"), userController.updateUserById);
+router.delete('/delete-user/:id', fileUpload("./storage/images"), userController.deleteUserById);
+
 
 
 export default router;
